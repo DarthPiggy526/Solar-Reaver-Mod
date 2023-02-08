a3568e817886c22e637e29217cf5df174d1671cb
Lysander Manius <AuSpecter3.0@outlook.com>
2023-02-08 06:57:27 -0500

    Comment changes
    Changed the "ELWP" comments at the tops to "TSR"

--- SR_Uniforms/Config.cpp
+++ SR_Uniforms/Config.cpp
@@ -1,11 +1,10 @@
 // For all 3 files replace:
-// "ELWP" with the full chapter name eg "ELWP"
-// "ELWP" with a shorthand chapter name, eg "ELWP"
-// "ELWP" with an acronym or short identifier, eg "ELWP"
+// "TSR" with the full chapter name eg "TSR"
+// "TSR" with a shorthand chapter name, eg "TSR"
+// "TSR" with an acronym or short identifier, eg "TSR"
 // These MUST be consistent throughout all 3 files or it will not function. Texture files are made using arma 3 tool's texview 2 saved as .paa.
 // DO NOT UNDER ANY CIRCUMSTANCE CHANGE ANYTHING THAT HAS "UM" OR "Ultra" IN IT, THESE ARE THE LINKS TO THE MODELS
 // Replace any mention of [ID] with your Chapter Prefix WITHOUT Brackets
-
 class CfgPatches
 {
 	class TIOW_Inf_SR
