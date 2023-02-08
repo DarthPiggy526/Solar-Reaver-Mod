8ee5a10ad9d1a343a72d2154a3c3f9942ee763fc
Lysander Manius <AuSpecter3.0@outlook.com>
2023-02-08 08:12:54 -0500

    File type change
    Changed all .bin Configs to .cpp files for ease of editing.
    
    Namely being:
    
    Arsenal
    
    SFX
    
    Vehicles
    
    WebknightsNarth

---
+++ SR_SFX/config.cpp
@@ -0,0 +1,509 @@
+class CfgPatches
+{
+	class sr_sfx
+	{
+		name="Solar Reavers Sound Effects Mod";
+		author="Tengu";
+		requiredAddons[]=
+		{
+			"A3_Sounds_F"
+		};
+		units[]=
+		{
+			"sr_module_electronic_alert_sys",
+			"sr_module_sfx_blue_alert",
+			"sr_module_sfx_red_alert",
+			"sr_module_sfx_intruder_alert",
+			"sr_module_sfx_evacuation_klaxon",
+			"sr_module_sfx_electrical_arcs",
+			"sr_module_sfx_trench_whistle",
+			"sr_module_sfx_nid_laugh",
+			"sr_module_sfx_nid_speech_1",
+			"sr_module_sfx_nid_speech_2",
+			"sr_module_sfx_nid_roar_1",
+			"sr_module_sfx_nid_roar_2",
+			"sr_module_sfx_nid_wheeze"
+		};
+		weapons[]=
+		{
+			""
+		};
+		magazines[]=
+		{
+			""
+		};
+		ammo[]=
+		{
+			""
+		};
+	};
+};
+class CfgSFX
+{
+	class sr_sfx_electronic_alert_sys
+	{
+		name="[SR] Electronic Alert System";
+		sounds[]=
+		{
+			"sound1"
+		};
+		sound1[]=
+		{
+			"sr_sfx\Sound\Electronic_Alert_system.ogg",
+			5,
+			1,
+			200,
+			1,
+			0,
+			0,
+			0
+		};
+		empty[]=
+		{
+			"",
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0
+		};
+	};
+	class sr_sfx_blue_alert
+	{
+		displayName="[SR] Blue Alert";
+		sounds[]=
+		{
+			"sound1"
+		};
+		sound1[]=
+		{
+			"sr_sfx\Sound\blue_alert.ogg",
+			5,
+			1,
+			200,
+			1,
+			0,
+			0,
+			0
+		};
+		empty[]=
+		{
+			"",
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0
+		};
+	};
+	class sr_sfx_red_alert
+	{
+		displayName="[SR] Red Alert";
+		sounds[]=
+		{
+			"sound1"
+		};
+		sound1[]=
+		{
+			"sr_sfx\Sound\red_alert.ogg",
+			5,
+			1,
+			200,
+			1,
+			0,
+			0,
+			0
+		};
+		empty[]=
+		{
+			"",
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0
+		};
+	};
+	class sr_sfx_intruder_alert
+	{
+		displayName="[SR] Intruder Alert";
+		sounds[]=
+		{
+			"sound1"
+		};
+		sound1[]=
+		{
+			"sr_sfx\Sound\intruder_alert.ogg",
+			5,
+			1,
+			200,
+			1,
+			0,
+			0,
+			0
+		};
+		empty[]=
+		{
+			"",
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0
+		};
+	};
+	class sr_sfx_evacuation_klaxon
+	{
+		displayName="[SR] Evacuation Klaxon";
+		sounds[]=
+		{
+			"sound1"
+		};
+		sound1[]=
+		{
+			"sr_sfx\Sound\evacuation_klaxon.ogg",
+			5,
+			1,
+			200,
+			1,
+			0,
+			0,
+			0
+		};
+		empty[]=
+		{
+			"",
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0
+		};
+	};
+	class sr_sfx_electrical_arcs
+	{
+		displayName="[SR] Electrical Arcs";
+		sounds[]=
+		{
+			"sound1"
+		};
+		sound1[]=
+		{
+			"sr_sfx\Sound\electrical_arcs.ogg",
+			5,
+			1,
+			200,
+			1,
+			0,
+			0,
+			0
+		};
+		empty[]=
+		{
+			"",
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0
+		};
+	};
+	class sr_sfx_trench_whistle
+	{
+		displayName="[SR] Trench Whistle";
+		sounds[]=
+		{
+			"sound1"
+		};
+		sound1[]=
+		{
+			"sr_sfx\Sound\trench_whistle.ogg",
+			5,
+			1,
+			50,
+			1,
+			999,
+			999,
+			999
+		};
+		empty[]=
+		{
+			"",
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0
+		};
+	};
+	class sr_sfx_nid_laugh
+	{
+		displayName="[SR] Nid Laugh";
+		sounds[]=
+		{
+			"sound1"
+		};
+		sound1[]=
+		{
+			"sr_sfx\Sound\nid_laugh.ogg",
+			5,
+			1,
+			50,
+			1,
+			999,
+			999,
+			999
+		};
+		empty[]=
+		{
+			"",
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0
+		};
+	};
+	class sr_sfx_nid_speech_1
+	{
+		displayName="[SR] Nid Speech 1";
+		sounds[]=
+		{
+			"sound1"
+		};
+		sound1[]=
+		{
+			"sr_sfx\Sound\nid_speech_1.ogg",
+			5,
+			1,
+			50,
+			1,
+			999,
+			999,
+			999
+		};
+		empty[]=
+		{
+			"",
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0
+		};
+	};
+	class sr_sfx_nid_speech_2
+	{
+		displayName="[SR] Nid Speech 2";
+		sounds[]=
+		{
+			"sound1"
+		};
+		sound1[]=
+		{
+			"sr_sfx\Sound\nid_speech_2.ogg",
+			5,
+			1,
+			50,
+			1,
+			999,
+			999,
+			999
+		};
+		empty[]=
+		{
+			"",
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0
+		};
+	};
+	class sr_sfx_nid_roar_1
+	{
+		displayName="[SR] Nid Roar 1";
+		sounds[]=
+		{
+			"sound1"
+		};
+		sound1[]=
+		{
+			"sr_sfx\Sound\nid_roar_1.ogg",
+			5,
+			1,
+			50,
+			1,
+			999,
+			999,
+			999
+		};
+		empty[]=
+		{
+			"",
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0
+		};
+	};
+	class sr_sfx_nid_roar_2
+	{
+		displayName="[SR] Nid Roar 2";
+		sounds[]=
+		{
+			"sound1"
+		};
+		sound1[]=
+		{
+			"sr_sfx\Sound\nid_roar_2.ogg",
+			5,
+			1,
+			50,
+			1,
+			999,
+			999,
+			999
+		};
+		empty[]=
+		{
+			"",
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0
+		};
+	};
+	class sr_sfx_nid_wheeze
+	{
+		displayName="[SR] Nid Wheeze";
+		sounds[]=
+		{
+			"sound1"
+		};
+		sound1[]=
+		{
+			"sr_sfx\Sound\nid_wheeze.ogg",
+			5,
+			1,
+			50,
+			1,
+			999,
+			999,
+			999
+		};
+		empty[]=
+		{
+			"",
+			0,
+			0,
+			0,
+			0,
+			0,
+			0,
+			0
+		};
+	};
+};
+class CfgVehicles
+{
+	class Sound;
+	class sr_sfx_electronic_alert_sys: Sound
+	{
+		author="Tengu";
+		scope=2;
+		sound="sr_sfx_electronic_alert_sys";
+		displayName="[SR] Electronic Alert System";
+	};
+	class sr_sfx_blue_alert: sr_sfx_electronic_alert_sys
+	{
+		sound="sr_sfx_blue_alert";
+		displayName="[SR] Blue Alert";
+	};
+	class sr_sfx_red_alert: sr_sfx_electronic_alert_sys
+	{
+		sound="sr_sfx_red_alert";
+		displayName="[SR] Red Alert";
+	};
+	class sr_sfx_intruder_alert: sr_sfx_electronic_alert_sys
+	{
+		sound="sr_sfx_intruder_alert";
+		displayName="[SR] Intruder Alert";
+	};
+	class sr_sfx_evacuation_klaxon: sr_sfx_electronic_alert_sys
+	{
+		sound="sr_sfx_evacuation_klaxon";
+		displayName="[SR] Evacuation Klaxon";
+	};
+	class sr_sfx_electrical_arcs: sr_sfx_electronic_alert_sys
+	{
+		sound="sr_sfx_electrical_arcs";
+		displayName="[SR] Electrical Arcs";
+	};
+	class sr_sfx_trench_whistle: sr_sfx_electronic_alert_sys
+	{
+		sound="sr_sfx_trench_whistle";
+		displayName="[SR] Trench Whistle";
+	};
+	class sr_sfx_nid_laugh: sr_sfx_electronic_alert_sys
+	{
+		sound="sr_sfx_nid_laugh";
+		displayName="[SR] Nid Laugh";
+	};
+	class sr_sfx_nid_speech_1: sr_sfx_electronic_alert_sys
+	{
+		sound="sr_sfx_nid_speech_1";
+		displayName="[SR] Nid Speech 1";
+	};
+	class sr_sfx_nid_speech_2: sr_sfx_electronic_alert_sys
+	{
+		sound="sr_sfx_nid_speech_2";
+		displayName="[SR] Nid Speech 2";
+	};
+	class sr_sfx_nid_roar_1: sr_sfx_electronic_alert_sys
+	{
+		sound="sr_sfx_nid_roar_1";
+		displayName="[SR] Nid Roar 1";
+	};
+	class sr_sfx_nid_roar_2: sr_sfx_electronic_alert_sys
+	{
+		sound="sr_sfx_nid_roar_2";
+		displayName="[SR] Nid Roar 2";
+	};
+	class sr_sfx_nid_wheeze: sr_sfx_electronic_alert_sys
+	{
+		sound="sr_sfx_nid_wheeze";
+		displayName="[SR] Nid Wheeze";
+	};
+};
+class cfgMods
+{
+	author="Tengu";
+	timepacked="1675255067";
+};
