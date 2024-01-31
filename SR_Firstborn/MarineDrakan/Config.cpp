7fe05755e02cf90af79b5fb10758db6ef0d3f8a6
Hummus <121640743+Hummuswithpaprika@users.noreply.github.com>
2024-01-31 16:08:52 +0200

    Drakan sholder pad(paa and code)

--- SR_Firstborn/MarineDrakan/Config.cpp
+++ SR_Firstborn/MarineDrakan/Config.cpp
@@ -19,6 +19,19 @@ class CfgPatches
 class CfgVehicles
 {
 	class TIOW_MK6_Tact_SR_BS;
+	class TIOW_MK7_Tact_SR_BS;
+	class TIOW_MK7_Tact_SR_Drakan: TIOW_MK7_Tact_SR_BS
+	{
+		author="Strixus";
+		displayName="Tactical Marine (MK. VII)";
+		uniformClass="SR_MK7_Armor_Drakan"; // This will be the classname of the PowerArmor, as seen in Weapons.HPP
+		hiddenSelectionsTextures[]=
+		{
+			"SR_FirstBorn\Textures\Drakan_Armour1.paa",
+			"SR_FirstBorn\Textures\SR_Armour2.paa"
+		};
+	};
+
 	class TIOW_MK6_Tact_SR_Drakan: TIOW_MK6_Tact_SR_BS
 	{
 		author="Strixus";
@@ -34,6 +47,7 @@ class CfgVehicles
 class CfgWeapons
 {
 	class ItemInfo;
+	class TIOW_MK7PowerArmor_1_WS;
 	class TIOW_MK6Helmet_UM;
 	class SR_MK6_Helmet_Drakan: TIOW_MK6Helmet_UM
 	{
@@ -42,6 +56,17 @@ class CfgWeapons
 		hiddenSelectionsTextures[]=
 		{
 			"SR_FirstBorn\Textures\Drakan_Helmet.paa"
+		};
+	};
+	class SR_MK2_Shoulders_Blackshield;
+	class SR_MK2_Shoulders_Drakan : SR_MK2_Shoulders_Blackshield
+	{
+		author="Strixus";
+		displayName="[TSR] MK 2 Pauldrons (Drakan)";
+		hiddenSelectionsTextures[] =
+		{
+			"SR_FirstBorn\Textures\Drakan_Armour1.paa",
+			"SR_FirstBorn\Textures\SR_Armour2.paa"
 		};
 	};
 };
